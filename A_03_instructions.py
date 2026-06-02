80b81f440593c2c240009b29a6b315cd4c1afd1a
Jeslezi <jesse.lin@kingsway.school.nz>
2026-06-02 21:23:11 +1200

    Code for instructions

---
+++ A_03_instructions.py
@@ -0,0 +1,22 @@
+instructions = input("\nPress enter for instructions or any other key to skip ")
+if instructions == "":
+    print("units for converting distance are")
+    print("mm")
+    print("cm")
+    print("m")
+    print("km") 
+
+instructions = input("\nPress enter for instructions or any other key to skip ")
+if instructions == "":
+    print("units for converting mass are")
+    print("kg")
+    print("g")
+    print("t")
+
+instructions = input("\nPress enter for instructions or any other key to skip ")
+if instructions == "":
+    print("units for converting time are")
+    print("s")
+    print("m")
+    print("h")
+    print("d") 
